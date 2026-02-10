ca4534599eb597695701ec479559075be1b689c4
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-02-10 20:48:18 +0000

    creacion de exception

--- src/EstacionClimatica.java
+++ src/EstacionClimatica.java
@@ -9,7 +9,13 @@ public class EstacionClimatica{
         temperaturas[i] = Math.random() * 40;
     }
     System.out.println("la temperatura del medio dia es:" + temperaturas[13] + "°c");
-
+    
+    try {
+        System.out.println("la temperatura en la hora 25 es:"+ temperaturas[24]+ "°c");
+    } catch (Exception e) {
+        // TODO: handle exception
+        
+    }
 }
 
 }
