e3ec73eef53a7220ac68223d2c6abea3ef5a9e05
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-02-13 02:41:26 +0000

    creacion de vectores

---
+++ SistemaBancarioLegacy/SistemaBancarioLegacy.java
@@ -0,0 +1,18 @@
+ import java.util.Vector;
+  
+ public class  SistemaBancarioLegacy {
+    public static void main(String[] args) {
+        Vector<String> logTransacciones = new Vector<>();
+
+        logTransacciones.add("transaccion #1");
+        logTransacciones.add("transaccion #2");
+        logTransacciones.add("transaccion #3");
+        logTransacciones.add("transaccion #4");
+        logTransacciones.add("transaccion #5");
+
+  //se bloquean los objetos para no cruzar hilos 
+
+    }
+
+    mensajes= 
+ }
